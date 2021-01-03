f6d11b4684a783d52c9c06612b821aeec1abf842
Alex Davis <davis.alex.jr@gmail.com>
2021-01-03 03:00:56 -0500

    Updated projects and code

---
+++ pls_dont_look_at_this_file.js
@@ -0,0 +1,3 @@
+// Ha made you look!
+// Now you have to hire me :)
+console.log(`%c I'm not fancy enough to have ascii art in the console but please accept this cute Lenny face. \n (◕‿◕✿)`, "font-family:monospace; font-size:16px");
